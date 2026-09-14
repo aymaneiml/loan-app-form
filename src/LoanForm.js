dd5b1f05d608abb4f8eee20911a0ab4fac2de9e9
aymaneIml <aymaneimlihi@gmail.com>
2026-09-14 21:58:14 +0200

    finalisation de projet

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,41 +1,111 @@
-import './FormStyles.css';
-import Modal from './Modal';
+import { useState } from "react";
+import "./FormStyles.css";
+import Modal from "./Modal";
 
-export default function LoanForm(){
+export default function LoanForm() {
 
-    return(
-        <div className="flex" style={{flexDirection:"column"}}>
+  
 
-           
+  const [loanInputs, setLoanInputs] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    isEmployee: false,
+    salaryRange: "",
+  });
 
-            <form id="loan-form" className="flex" style={{ flexDirection:"column"}}>
-                <h1>Requesting a Loan</h1>
+  const btnIsDisabled =
+    loanInputs.name === "" ||
+    loanInputs.age === "" ||
+    loanInputs.phoneNumber === "";
 
-                <hr></hr>
 
-                <label>Name:</label>
-                <input />
+  const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null)
 
-                <label>Phone Number:</label>
-                <input />
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    setErrorMessage(null);
 
-                <label>Age:</label>
-                <input />
+    if(loanInputs.age < 18 || loanInputs.age > 100){
+      setErrorMessage("The age is not allowed")
+    } else if(loanInputs.phoneNumber.length<10 || loanInputs.phoneNumber.length>12){
+      setErrorMessage("Phone Number Format Is Incorrect !!")
+    }
 
-                <label style={{ marginTop:"30px"}}>Are you an employee?</label>
-                <input type="checkbox"/>
+    setShowModal(true)
+  }
 
-                <label>Salary:</label>
-                <select>
-                    <option>less than 500$</option>
-                    <option>between 500$ and 2000$</option>
-                    <option>above 2000$</option>
-                </select>
+  function handleDivClick(){
+    if(showModal){
+      setShowModal(false);
+    } 
+  }
 
-                <button id="submit-loan-btn">Submit</button>
-            </form>
 
-            <Modal/>
-        </div>
-    );
+  return (
+    <div onClick={handleDivClick} className="flex" style={{ flexDirection: "column" }}>
+      <form id="loan-form" className="flex" style={{ flexDirection: "column" }}>
+        <h1>Requesting a Loan</h1>
+
+        <hr></hr>
+
+        <label>Name:</label>
+        <input
+          value={loanInputs.name}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, name: event.target.value });
+          }}
+        />
+        
+        <label>Phone Number:</label>
+        <input
+          value={loanInputs.phoneNumber}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, phoneNumber: event.target.value });
+          }}
+        />
+
+        <label>Age:</label>
+        <input
+          value={loanInputs.age}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, age: event.target.value });
+          }}
+        />
+
+        <label style={{ marginTop: "30px" }}>Are you an employee?</label>
+        <input
+          type="checkbox"
+          checked={loanInputs.isEmployee}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, isEmployee: event.target.checked });
+          }}
+        />
+
+        <label>Salary:</label>
+        <select
+          value={loanInputs.salaryRange}
+          onChange={(event) => {
+            setLoanInputs({ ...loanInputs, salaryRange: event.target.value });
+          }}
+        >
+          <option>less than 500$</option>
+          <option>between 500$ and 2000$</option>
+          <option>above 2000$</option>
+        </select>
+
+        <button
+          className={btnIsDisabled ? "disabled" : ""}
+          disabled={btnIsDisabled}
+          id="submit-loan-btn"
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </button>
+      </form>
+
+      <Modal isVisible={showModal} errorMessage={errorMessage}/>
+    </div>
+  );
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,18 @@
-
-export default function Modal(){
-
-    return(
-        <div id="modal">
-            <div id="modal-content">
-                <h1>The Form Has been Submitted Successfully</h1>
-            </div>
+export default function Modal({ isVisible, errorMessage = null }) {
+  if (isVisible) {
+    return (
+      <div id="modal">
+        <div id="modal-content">
+          {/* <h1>The Form Has been Submitted Successfully</h1> */}
+          <h1 style={{color: errorMessage != null ? "red" : "green"}}>
+            {errorMessage != null
+              ? errorMessage
+              : "The Form Has been Submitted Successfully"}
+          </h1>
         </div>
+      </div>
     );
+  } else {
+    return <></>;
+  }
 }
